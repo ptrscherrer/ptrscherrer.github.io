1fb756caec127fd26c9625e5792f14ac5826334c
ptrscherrer <ptrscherrer@gmail.com>
2017-07-14 23:55:25 +0800

    added Chinese

--- js/PandPscript.js
+++ js/PandPscript.js
@@ -1,3 +1,22 @@
+	
+$(document).ready(function(){
+  var $sections = $('.section');
+  $(window).scroll(function(){
+    var currentScroll = $(this).scrollLeft();
+    var $currentSection
+    $sections.each(function(){      
+      var divPosition = $(this).offset().left;
+      if( divPosition - 1 < currentScroll ){
+        $currentSection = $(this);
+      }
+      var id = $currentSection.attr('id');
+   	 $('a').removeClass('active');
+   	 $("[href=#"+id+"]").addClass('active');
+      
+    })
+
+  });
+});
 
 $(document).ready(function(){
 	$('ul.nav a').on('click',function(event){
@@ -9,23 +28,49 @@ $(document).ready(function(){
 		event.preventDefault();
 	});});
 
-$(function(){
-	$(".hide").hide();
+	
 
-$("#pete").click(function(){
-	$("#peted").show();
-	$(".hide2").hide();
-});
+		$(function(){
+			$(".hide, .hide3").hide();
 
-$("#phoeb").click(function(){
-	$("#phoebd").show();
-	$(".hide1").hide();
-});
-$("#us").click(function(){
-	$("#usd").show();
-	$(".hide").hide();
+		$("#pete").click(function(){
+			$("#peted").show();
+			$(".hide2").hide();
+		});
 
-});
+		$("#phoeb").click(function(){
+			$("#phoebd").show();
+			$(".hide1").hide();
+		});
+		$("#us").click(function(){
+			$("#usd").show();
+			$(".hide").hide();
 
+		});
 
-});
+		$("#petec").click(function(){
+			$("#petedc").show();
+			$(".hide2").hide();
+		});
+
+		$("#phoebc").click(function(){
+			$("#phoebdc").show();
+			$(".hide1").hide();
+		});
+		$("#usc").click(function(){
+			$("#usdc").show();
+			$(".hide").hide();
+
+		});
+
+		$("#man").click(function(){
+			$(".chinese, #usdc").show();
+			$(".english").hide();
+		});
+
+		$("#eng").click(function(){
+			$(".english, #usd").show();
+			$(".chinese").hide();
+		});
+
+		});
